06208571591ece40c7d9bd7e30b56055e2b4c731
Space Cat Atelier <139983936+Space-Cat-Atelier@users.noreply.github.com>
2024-06-25 14:19:56 +0500

    Update Autoclicker.py
    Added ability to choose mouse button

--- Autoclicker/Autoclicker.py
+++ Autoclicker/Autoclicker.py
@@ -26,13 +26,27 @@ def callback(val):
             return True
         return False
 
+def focus_out(event):
+    if event.widget is tk:
+        tk.focus_set()
+
 reg = tk.register(callback)
 delay_label = Label(tk, height=2, width=11, text='Click Delay\nin Milliseconds', bg='#1e1e1e', fg='white')
 delay_input = Entry(tk, textvariable=delay_val, width=22, validate='key', validatecommand=(reg, '%P'))
+tk.bind("<Button-1>", focus_out)
+
+options = ['Left', 'Middle', 'Right']
+mouse_button = StringVar()
+mouse_button.set(options[0])
+mouse_label = Label(tk, height=2, width=11, text='Mouse\nButton', bg='#1e1e1e', fg='white')
+mouse_select = OptionMenu(tk, mouse_button, *options)
+mouse_select.config(width=15)
 
 start_btn.place(x=15, y=15)
 start_key_label.place(x=100, y=16)
 delay_label.place(x=5, y=75)
 delay_input.place(x=100, y=83)
+mouse_label.place(x=5, y=125)
+mouse_select.place(x=100, y=125)
 
 tk.mainloop()
