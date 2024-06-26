dccabe06245252627d816c291d7a03177ac0b96f
Space Cat Atelier <139983936+Space-Cat-Atelier@users.noreply.github.com>
2024-06-26 13:12:08 +0500

    Update Autoclicker.py
    Removed some code and need to add threading

--- Autoclicker/Autoclicker.py
+++ Autoclicker/Autoclicker.py
@@ -10,8 +10,11 @@ tk.configure(bg='#1e1e1e')
 
 start_key_var = StringVar()
 start_key_var.set('F5')
+start_text = StringVar()
+start_text.set('Press '+start_key_var.get()+'\nto Start')
 def get_key():
     start_key_var.set(keyboard.read_key().upper())
+    start_text.set('Press '+start_key_var.get()+'\nto Start')
 
 start_key_btn = Button(tk, text='Key Select', height=2, command=get_key)
 start_key_label = Label(tk, height=2, width=16, font=('serif', 10, 'bold'), textvariable=start_key_var)
@@ -43,15 +46,7 @@ mouse_label = Label(tk, height=2, width=11, text='Mouse\nButton', bg='#1e1e1e', 
 mouse_select = OptionMenu(tk, mouse_button, *options)
 mouse_select.config(width=15)
 
-start_var = StringVar()
-start_var.set('Start')
-def start_stop():
-    if start_var.get() == 'Start':
-        start_var.set('Stop')
-    elif start_var.get() == 'Stop':
-        start_var.set('Start')
-
-start_btn = Button(tk, textvariable=start_var, height=3, width=17, command=start_stop, font=('serif', 16))
+start_label = Label(tk, height=2, width=11, textvariable=start_text, bg='#1e1e1e', fg='white', font=(30))
 
 start_key_btn.place(x=15, y=15)
 start_key_label.place(x=100, y=16)
@@ -59,6 +54,6 @@ delay_label.place(x=5, y=75)
 delay_input.place(x=100, y=83)
 mouse_label.place(x=5, y=125)
 mouse_select.place(x=100, y=125)
-start_btn.place(x=19, y=190)
+start_label.place(x=70, y=200)
 
 tk.mainloop()
